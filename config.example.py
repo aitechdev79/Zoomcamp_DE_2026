8c182158d5c5f32d134e497a3f38e3770625b7c7
aitechdev79 <aitechdev79@gmail.com>
2026-05-05 05:09:13 +0700

    Create config.example.py

---
+++ config.example.py
@@ -0,0 +1,13 @@
+AWS_REGION = "ap-southeast-1"
+S3_BUCKET = "hoa-era5-climate-lake"
+S3_STAGING_DIR = "s3://hoa-era5-climate-lake/athena-results/"
+DATABASE = "climate_analytics"
+TABLE = "era5_vietnam_daily"
+
+YEAR = 2023
+MONTH = 1
+
+LAT_MIN = 8.0
+LAT_MAX = 24.0
+LON_MIN = 102.0
+LON_MAX = 110.0
